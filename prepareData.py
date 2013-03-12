0c0768f7ad3926398feb7dccaf3e7251fbaa1e37
zcgzcgzcg <zcg.cs60@gmail.com>
2013-03-12 14:54:09 -0700

    analysis program

--- prepareData.py
+++ prepareData.py
@@ -1,6 +1,54 @@
 from fuel.models import *
 import time
 import datetime
+from sets import Set
+
+print 'Outputing all project members...'
+f = open('projectmember.txt','w')
+for p in Project.objects.all():
+    a = p.members.all()
+    f.write(str(len(a)))
+    for m in a:
+        f.write(' ' + str(m.id))
+    f.write('\n')
+f.close()
+
+f = open('projecthighscore.txt','w')
+for p in Project.objects.all():
+    a = p.members.all()
+    for m in a:
+        s = Score.objects.filter(user=m)
+        if len(s) > 0:
+            scores = json.loads(s[0].score)
+            f.write(str(m.id))
+            for pid in range(1,14):
+                if str(pid) in scores:
+                    if scores[str(pid)] == 5:
+                        f.write(' ' + str(pid))
+            f.write('\n')
+                       # print str(m) + ' gives 5 for project ' + str(pid)
+f.close()
+
+
+print 'Outputing all scales...'
+f = open('scale.txt', 'w')
+for s in Scale.objects.all().reverse():
+    if not s.active:
+        #rank of the winner w.r.t points earned up to the winning time
+        rank = 1
+        winner_point = sum([a.amount for a in Amount.objects.filter(user=s.winner).filter(time__lt=s.get_end_time()).filter(amount__gt=0)])
+        for u in User.objects.all():
+            u_point = sum([a.amount for a in Amount.objects.filter(user=u).filter(time__lt=s.get_end_time()).filter(amount__gt=0)])
+            if u_point > winner_point:
+                rank = rank + 1
+
+        f.write(str(time.mktime(s.start_time.timetuple()))+ ' ' + str(time.mktime(s.get_end_time().timetuple())) + ' ' + str(s.winner.id) + ' ' +str(rank) + ' ' + str(s.money) + ' ' + str(len(s.amount_set.all())) + ' ')
+        participate = Set([])
+        for a in s.amount_set.all():
+            participate.add(a.user.id)
+        f.write(str(len(participate)) + '\n')
+f.close()
+
 
 print 'Outputing all amounts...'
 f = open('amount.txt','w')
